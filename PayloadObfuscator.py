c65efb82bf73d86b1434dfaeea17478f9b35b92e
Joel Verghese <51381507+krizzsk@users.noreply.github.com>
2023-10-06 13:33:13 +0530

    Update PayloadObfuscator.py

--- PayloadObfuscator.py
+++ PayloadObfuscator.py
@@ -5,8 +5,7 @@ class PayloadObfuscator:
         double_encoded = urllib.parse.quote(urllib.parse.quote(payload))
         html_encoded = payload.encode("ascii", "xmlcharrefreplace").decode("ascii")
         leading_zeros = "0" * 100 + payload
-        xml_encoded = payload.encode("ascii", "xmlcharrefreplace").decode("ascii")
         unicode_escaped = payload.encode("unicode-escape").decode("utf-8")
         hex_escaped = payload.encode("utf-8").hex()
 
-        return [double_encoded, html_encoded, leading_zeros, xml_encoded, unicode_escaped, hex_escaped]
+        return [double_encoded, html_encoded, leading_zeros, unicode_escaped, hex_escaped]
